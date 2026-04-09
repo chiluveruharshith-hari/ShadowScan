92c8ac470cedb6b7278baf2e2e44533a8469d60d
kushalkasparaju-star <kushalkasparaju@gmail.com>
2026-04-09 20:39:03 +0530

    Kushal-commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from .modules.url_module.route import router as url_router
+from .modules.phone_module.route import router as phone_router   # ✅ ADD THIS
 from dotenv import load_dotenv
+
 load_dotenv()
 
+app = FastAPI(title="ShadowScan API")
 
-app = FastAPI(title="ShadowScan URL Analysis")
-
-# Register the URL analysis router
+# Register routers
 app.include_router(url_router)
+app.include_router(phone_router)  
 
 @app.get("/")
 def read_root():
-    return {"message": "ShadowScan URL Analysis API is active"}
+    return {"message": "ShadowScan API is active"}
